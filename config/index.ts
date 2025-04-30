afe03c00219431ed8f1b2878c2ec2dd7d84f795d
Yatit Thakker <yatit.thakker@jhilburn.com>
2025-04-30 11:14:36 -0500

    Update title

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'JESSI 2.0',
+  title: 'JESSI',
   description: 'The J.Hilburn Educational Stylist Support Interface',
   copyright: 'J.Hilburn',
   privacy_policy: '',
